fd2605b76129f59b64a3eebf9b20d916aedfc574
piflynn <piflynn@me.com>
2018-04-14 23:36:49 -0400

    Add inventory list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/models/inventory-item.ts
@@ -0,0 +1,7 @@
+export interface IInventoryItem {
+    name: string;
+    category: string;
+    description: string;
+    purchasePrice: number;
+    rentalCount: number;
+}
